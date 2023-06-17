5adc35360de1961fe3ae0ff94ea74cd27b77e8a4
Gidrevich Anton <Tony.gidrevich@mail.ru>
2023-06-17 13:52:32 +0300

    feat:edit task3 & done extra

--- task3.py
+++ task3.py
@@ -3,12 +3,17 @@
 # “Число является простым, если делится нацело только на единицу и на себя”.
 # Сделайте ограничение на ввод отрицательных чисел и чисел больше 100 тысяч.
 
-
-number = 6
 NUMBER_EXCEPTION_1 = 2
 NUMBER_EXCEPTION_2 = 3
-if number % 1 == 0 and number % number == 0 \
-        and (number % NUMBER_EXCEPTION_1 == 0 or number % NUMBER_EXCEPTION_2 == 0):
-    print("Число составное")
-elif number % 1 == 0 and number % number == 0:
-    print("Число простое")
+LEFT_BORDER = 0
+RIGHT_BORDER = 100000
+number = 5
+
+if LEFT_BORDER < number < RIGHT_BORDER:
+    if number % 1 == 0 and number % number == 0 \
+            and (number % NUMBER_EXCEPTION_1 == 0 or number % NUMBER_EXCEPTION_2 == 0):
+        print("Число составное")
+    elif number % 1 == 0 and number % number == 0:
+        print("Число простое")
+else:
+    print("Out of range!")
