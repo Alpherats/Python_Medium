d447f5786d25784039d59fd4abcc6a9da24bc034
Gidrevich Anton <Tony.gidrevich@mail.ru>
2023-07-25 15:10:28 +0300

    feat:done task 1 & 2

--- hometasks/task1.py
+++ hometasks/task1.py
@@ -5,3 +5,27 @@
 # Отдельно сообщить является ли треугольник разносторонним, равнобедренным или равносторонним.
 
 
+class TypeOfTriangle:
+    def __init__(self, a: int, b: int, c: int):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def solve_triangle(self):
+        if self.a < self.b + self.c and self.b < self.a + self.c and self.c < self.a + self.b:
+            if self.a != self.b and self.b != self.c and self.a != self.c:
+                return f'Есть разностороний со сторонами - {self.a}, {self.b}, {self.c}'
+            elif self.a == self.b and self.b == self.c and self.a == self.c:
+                return f'Есть равносторонний со сторонами - {self.a}, {self.b}, {self.c}'
+            elif self.a == self.b or self.a == self.c or self.b == self.c:
+                return f'Есть равнобедренный со сторонами - {self.a}, {self.b}, {self.c}'
+        else:
+            return f"Нет такого треугольника"
+
+
+
+first = TypeOfTriangle(10, 5, 6)
+print(first.solve_triangle())
+
+second = TypeOfTriangle(2,2,2)
+print(second.solve_triangle())
