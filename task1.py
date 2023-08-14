e01c5180e5413e206382308f8b742155ccf2d7ae
Gidrevich Anton <Tony.gidrevich@mail.ru>
2023-08-14 20:00:21 +0300

    done tasks

--- task1.py
+++ task1.py
@@ -1,24 +1,79 @@
-import re
 import doctest
+import unittest
 
 
-def some_txt(string: str) -> str:
-    """
-    Убирает из строки все нелатинское
 
-    # возврат в нижнем регистре
-    >>> some_txt('Hdkfps df')
-    'hdkfps df'
 
-    # возврат без кириллицы
-    >>> some_txt("привет sthrn")
-    ' sthrn'
-    """
+class Rectangle:
+    '''
+    Класс для создания прямоугольников с заданием сторон, нахождения
+    периметра, площади и их сравнения
+    '''
 
-    return re.sub(r'[^a-zA-Z ]', '', string).lower()
+    def __init__(self, length: float, width: float or None = None) -> None:
+        self.length = length
+        if width:
+            self.width = width
+        else:
+            self.width = length
+
+    def per(self) -> float:
+        return self.length * 2 + self.width * 2
+
+    def square(self) -> float:
+        return self.length * self.width
+
+    def __add__(self, other) -> float or int:
+        sum_of_per = self.per() + other.per()
+        width_rect_c = self.width
+        length_rect_c = sum_of_per / 2 - width_rect_c
+
+        return Rectangle(width_rect_c, length_rect_c)
+
+    def __sub__(self, other):
+        min_of_per = abs(self.per() - other.per())
+        width_rec_c = min(self.width, other.width, self.length, other.length)
+        length_rec_c = min_of_per / 2 - width_rec_c
+        return Rectangle(width_rec_c, length_rec_c)
+
+    def __eq__(self, other):
+        return self.square() == other.square()
+
+    def __ne__(self, other):
+        return self.square() != other.square()
+
+    def __gt__(self, other):
+        return self.square() > other.square()
+
+    def __ge__(self, other):
+        return self.square() >= other.square()
+
+    def __lt__(self, other):
+        return self.square() < other.square()
+
+    def __le__(self, other):
+        return self.square() <= other.square()
+
+
+class TestRectangle(unittest.TestCase):
+    def setUp(self) -> None:
+        self.rec1 = Rectangle(1, 1)
+        self.rec2 = Rectangle(2, 2)
+        self.rec3 = Rectangle(2, 4)
+
+    def test_sum_perim(self):
+        self.assertEqual(Rectangle(2, 8), self.rec2 + self.rec3)
+
+    def test_calc_area(self):
+        self.assertEqual(3, self.rec2.square(),
+                         'проверьте заданную площадь ')
+
+    def test_equal_recs(self):
+        self.assertFalse(self.rec1 == self.rec2)
 
 
 if __name__ == '__main__':
-    doctest.testmod(verbose=True)
+    unittest.main()
 
-#
+
+
